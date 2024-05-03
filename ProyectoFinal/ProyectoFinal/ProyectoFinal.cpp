ec5df074954a8a27401656804d7a20058503adce
Hectorchv <hectorchv045@gmail.com>
2024-05-02 18:46:45 -0600

    Add pine model
    Se agrega el modelo del pino

--- ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
@@ -62,6 +62,7 @@ Model CarroCoraje_M;
 Model Coraje_M;
 Model CasaCoraje_M;
 Model Lampara_M;
+Model Pino_M;
 
 Skybox skybox;
 
@@ -285,6 +286,8 @@ int main()
 	CasaCoraje_M.LoadModel("Models/Coraje/casaCoraje.obj");
 	Lampara_M = Model();
 	Lampara_M.LoadModel("Models/lampara.obj");
+	Pino_M = Model();
+	Pino_M.LoadModel("Models/pino.obj");
 
 	// Skybox
 	std::vector<std::string> skyboxFaces;
@@ -486,6 +489,13 @@ int main()
 		model = glm::scale(model, glm::vec3(6.0f, 6.0f, 6.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		Lampara_M.RenderModel();
+
+		//Instancia Pinos
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(50.0f, -1.0f, -60.0f));
+		//model = glm::scale(model, glm::vec3(6.0f, 6.0f, 6.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Pino_M.RenderModel();
 
 
 		//Agave ¿qué sucede si lo renderizan antes del coche y el helicóptero?
